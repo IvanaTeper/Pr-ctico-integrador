92a24f73217a8ee206c2e730b249b4de7eedf969
nfloresmuino <143338132+nfloresmuino@users.noreply.github.com>
2024-05-24 14:31:06 -0300

    Update formulario.js

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -1,18 +1,18 @@
-            // Deshabilitamos el envio de formularios si hay campo no válidos (función autoinvocada):
-            (() => {
-                'use strict'
+// Deshabilitamos el envio de formularios si hay campo no válidos (función autoinvocada):
+(() => {
+            'use strict'
             
-                // Aplicamos al formulario definido en fila 72 la siguiente sentencia, la usamos para formularios que necesitan validación:
+// Aplicamos al formulario definido en fila 72 la siguiente sentencia, la usamos para formularios que necesitan validación:
                 
-                const forms = document.querySelectorAll('.needs-validation')
+            const forms = document.querySelectorAll('.needs-validation')
             
-                // Iteramos sobre los formularios que obtuvimos en el paso anterior:
+// Iteramos sobre los formularios que obtuvimos en el paso anterior:
     
-                Array.from(forms).forEach(form => {
-                form.addEventListener('submit', event => {
-                    if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
+            Array.from(forms).forEach(form => {
+            form.addEventListener('submit', event => {
+                        if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
                     }
             
                     form.classList.add('was-validated')
